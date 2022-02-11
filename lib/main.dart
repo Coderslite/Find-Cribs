2961c96a1c5b8b97b7d4241c7fa52fcc662a53f2
ChineduG <investorgbriel@gmail.com>
2022-02-11 15:22:28 +0100

    authentication screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'authentication_screen/sign_in_verify_email_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,8 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: VerifyEmailScreen());
   }
 }
