0ff085257a7be48745750e691119d5fc9b0c05b0
Miikhaaeel <fadowomichael1@gmail.com>
2022-02-11 18:54:16 +0100

    onboarding Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:find_cribs/screens/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/onboarding.dart';
 
 void main() {
@@ -10,14 +12,21 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        //color set to transperent
+      ),
+    );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'FindCribs',
       theme: ThemeData(),
       home: AnimatedSplashScreen(
-        duration: 5000,
+        duration: 3800,
         splashIconSize: double.maxFinite,
         splash: 'assets/images/splash_screen.gif',
-        nextScreen: OnboardingScreen(),
+        nextScreen: const OnboardingScreen(),
         backgroundColor: const Color(0xFF0070B9),
         centered: true,
       ),
